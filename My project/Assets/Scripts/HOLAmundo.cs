404e78bd739b8ef5e20863c647e5336c5dd9ffe2
Abraham Merchan <abrahammerchan1806@gmail.com>
2025-10-23 21:47:46 -0600

    actualizacions de la primera version

---
+++ My project/Assets/Scripts/HOLAmundo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HOLAmundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       print("Hola mundo");
+        Debug.Log("algo paso");
+        Debug.LogWarning("algo salio medianamente mal");
+        Debug.LogError("algo salio muy mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
